c494f698807a7fa16f1eaca31b248805dd495a0b
Hopt Dev <hoptdev@outlook.com>
2024-06-21 12:37:03 +0500

    Remove trash

--- EDevHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EDevHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
